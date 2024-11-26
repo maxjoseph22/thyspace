c3454a97fb53d4bd9bc90079356f00d7065f2c3a
safaa <safaa.imran7@gmail.com>
2024-11-26 17:01:58 +0000

    completed model tests

--- api/tests/models/post.test.js
+++ api/tests/models/post.test.js
@@ -1,24 +1,30 @@
 require("../mongodb_helper");
-
+const mongoose = require("mongoose");
 const Post = require("../../models/post");
 
 describe("Post model", () => {
+
+  let mockUserId
   beforeEach(async () => {
     await Post.deleteMany({});
+    mockUserId = new mongoose.Types.ObjectId();
   });
 
   it("creates a post", () => {
-    const post = new Post({ message: "some message", 
-                            user_id: "12384hjfed", 
-                            image: "", 
-                            likes: [], 
-                            comments: []
-    });
-    expect(post).toEqual({message: "some message", 
-      user_id: "12384hjfed", 
+
+    const post = new Post({
+      message: "some message", 
+      user_id: mockUserId, 
       image: "", 
       likes: [], 
-      comments: []});
+      comments: []
+    });
+
+    expect(post.message).toEqual("some message");
+    expect(post.user_id).toEqual(mockUserId);
+    expect(post.image).toEqual("");
+    expect(post.likes).toEqual([]);
+    expect(post.comments).toEqual([]);
   });
 
   it("can list all posts", async () => {
@@ -28,29 +34,30 @@ describe("Post model", () => {
 
   it("can save a post", async () => {
     const post = new Post({ message: "some message", 
-      user_id: "12384hjfed", 
+      user_id: mockUserId, 
       image: "", 
       likes: [], 
       comments: [] });
 
     await post.save();
     const posts = await Post.find();
-    expect(posts[0]).toEqual({message: "some message", 
-      user_id: "12384hjfed", 
-      image: "", 
-      likes: [], 
-      comments: []});
+    
+    expect(posts[0].message).toEqual("some message");
+    expect(posts[0].user_id).toEqual(mockUserId);
+    expect(posts[0].image).toEqual("");
+    expect(posts[0].likes).toEqual([]);
+    expect(posts[0].comments).toEqual([]);
   });
 
   it("can save multiple posts", async () => {
     const post1 = new Post({ message: "some message", 
-      user_id: "12384hjfed", 
+      user_id: mockUserId, 
       image: "", 
       likes: [], 
       comments: []});
 
     const post2 = new Post({ message: "second message", 
-      user_id: "12384hjfed", 
+      user_id: mockUserId, 
       image: "", 
       likes: [], 
       comments: [] });
@@ -59,24 +66,23 @@ describe("Post model", () => {
     await post2.save();
 
     const posts = await Post.find();
+    expect(posts[0].message).toEqual("some message");
+    expect(posts[0].user_id).toEqual(mockUserId);
+    expect(posts[0].image).toEqual("");
+    expect(posts[0].likes).toEqual([]);
+    expect(posts[0].comments).toEqual([]);
 
-    expect(posts).toEqual([
-      {message: "some message", 
-      user_id: "12384hjfed", 
-      image: "", 
-      likes: [], 
-      comments: []}, 
-
-      {message: "second message", 
-        user_id: "12384hjfed", 
-        image: "", 
-        likes: [], 
-        comments: []}]);
+    expect(posts[1].message).toEqual("second message");
+    expect(posts[1].user_id).toEqual(mockUserId);
+    expect(posts[1].image).toEqual("");
+    expect(posts[1].likes).toEqual([]);
+    expect(posts[1].comments).toEqual([]);
+    
   });
 
   it("update a post", async () => {
     const post = new Post({message: "some message", 
-      user_id: "12384hjfed", 
+      user_id: mockUserId, 
       image: "", 
       likes: [], 
       comments: []});
@@ -87,24 +93,24 @@ describe("Post model", () => {
     await post.save();
     
     const posts = await Post.find();
-    expect(posts).toEqual([
-      {message: "new message", 
-        user_id: "12384hjfed", 
-        image: "", 
-        likes: [], 
-        comments: []}
-    ])
+    
+    expect(post.message).toEqual("new message");
+    expect(post.user_id).toEqual(mockUserId);
+    expect(post.image).toEqual("");
+    expect(post.likes).toEqual([]);
+    expect(post.comments).toEqual([]);
+
   })
 
   it("update a post with multiple posts in the database", async () => {
     const post1 = new Post({message: "some message", 
-      user_id: "12384hjfed", 
+      user_id: mockUserId, 
       image: "", 
       likes: [], 
       comments: []});
 
     const post2 = new Post({message: "second message", 
-      user_id: "12384hjfed", 
+      user_id: mockUserId, 
       image: "", 
       likes: [], 
       comments: []});
@@ -119,20 +125,18 @@ describe("Post model", () => {
     const postOne = await Post.find({message: "new message"});
     const postTwo = await Post.find({message: "second message"});
     const posts = await Post.find();
+    
+    expect(post2.message).toEqual("second message");
+    expect(post2.user_id).toEqual(mockUserId);
+    expect(post2.image).toEqual("");
+    expect(post2.likes).toEqual([]);
+    expect(post2.comments).toEqual([]);
 
-    expect(postTwo).toEqual({message: "second message", 
-      user_id: "12384hjfed", 
-      image: "", 
-      likes: [], 
-      comments: []});
-
-    expect(postOne).toEqual([{
-        message: "new message", 
-        user_id: "12384hjfed", 
-        image: "", 
-        likes: [], 
-        comments: []}
-    ])
+    expect(post1.message).toEqual("new message");
+    expect(post1.user_id).toEqual(mockUserId);
+    expect(post1.image).toEqual("");
+    expect(post1.likes).toEqual([]);
+    expect(post1.comments).toEqual([]);
 
     expect(posts.length).toEqual(2);
   })
@@ -140,7 +144,7 @@ describe("Post model", () => {
   it("deletes a post", async () => {
     const post = new Post({
       message: "some message", 
-      user_id: "12384hjfed", 
+      user_id: mockUserId, 
       image: "", 
       likes: [], 
       comments: []})
@@ -155,14 +159,14 @@ describe("Post model", () => {
   it("definitely deletes one instead of all of them", async () => {
     const post1 = new Post({
       message: "some message", 
-      user_id: "12384hjfed", 
+      user_id: mockUserId, 
       image: "", 
       likes: [], 
       comments: []
     })
     const post2 = new Post({
       message: "second message", 
-      user_id: "12384hjfed", 
+      user_id: mockUserId, 
       image: "", 
       likes: [], 
       comments: []
@@ -175,12 +179,11 @@ describe("Post model", () => {
     const posts = await Post.find();
 
     expect(posts.length).toEqual(1);
-    expect(posts).toEqual([{
-      message: "second message", 
-      user_id: "12384hjfed", 
-      image: "", 
-      likes: [], 
-      comments: []
-    }])
+    
+    expect(posts[0].message).toEqual("second message");
+    expect(posts[0].user_id).toEqual(mockUserId);
+    expect(posts[0].image).toEqual("");
+    expect(posts[0].likes).toEqual([]);
+    expect(posts[0].comments).toEqual([]);
   })
 });
