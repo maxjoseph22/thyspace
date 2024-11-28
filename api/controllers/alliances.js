497e8fc42e817b11b2deb9e18e3698bac20212ef
Khalid Ham <Khalidoham@gmail.com>
2024-11-28 11:55:17 +0000

    Users can request and withdraw alliances

--- api/controllers/alliances.js
+++ api/controllers/alliances.js
@@ -15,8 +15,29 @@ const requestAlliance = async (req, res) => {
     }
 } 
 
+const withdrawAllianceRequest = async (req, res) => {
+    try {
+        const sender = req.body.sender
+        const receiver = req.params.id
+
+        const alliance = await Alliance.findOne({sender: sender, receiver: receiver})
+    
+        if (alliance.status !== 'pending') {
+            res.status(403).json({message: "You may not withdraw a forged alliance! Only spilled blood can unforge this alliance."})
+        }
+
+        await Alliance.deleteOne({ _id: alliance._id })
+        res.status(201).json({message: "Alliance request successfully withdrawn!"})
+
+    } catch (error) {
+        console.log(`\n${error.message}\n`)
+        res.status(500).json({message: "An error occured, alliance not withdrawn!"})
+    }
+}
+
 const AllianceController = {
-    requestAlliance: requestAlliance
+    requestAlliance: requestAlliance,
+    withdrawAllianceRequest: withdrawAllianceRequest
 }
 
 module.exports = { AllianceController }
