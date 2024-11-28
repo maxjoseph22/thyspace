898d30e9dcac96860dfde94ee186cf025c34731f
Doug Fairfield <fairfielddouglas@gmail.com>
2024-11-28 12:35:34 +0000

    CRUD controllers and tests so far

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,26 +1,96 @@
 const User = require("../models/user");
 
-function create(req, res) {
-  const email = req.body.email;
-  const password = req.body.password;
-  const username = req.body.username
-  const location = req.body.location
+// createUser function
+async function createUser(req, res) {
+  try {
+    const {
+      username,
+      email,
+      password,
+      location,
+      firstname,
+      lastname,
+      profilePicture,
+      alliances = [],
+      posts = [],
+    } = req.body;
 
-  const user = new User({ email, password, username, location });
-  user
-    .save()
-    .then((user) => {
-      console.log("User created, id:", user._id.toString());
-      res.status(201).json({ message: "OK" });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(400).json({ message: "Something went wrong" });
-    });
+  if (!username || !email || !password || !firstname || !lastname) {
+    return res.status(400).json({ message: "All required fields must be filled"})
+  }
+  const user = new User({
+    email, 
+    password, 
+    username, 
+    location, 
+    firstname,
+    lastname,
+    profilePicture,
+    alliances,
+    posts });
+
+    const savedUser = await user.save()
+
+    if (!savedUser) {
+      return res.status(409).json({message: "User already exists"})
+    }
+    res.status(201).json({message: "User successfully created!"})
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Unexpected server error" });
+  }
+  }
+
+// delete function
+async function deleteUser(req, res) {
+  try {
+    const userId = req.params.id
+    const deletedUser = await User.findByIdAndDelete(userId)
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
+  }
+  res.status(200).json({message: "User deleted", user: deletedUser}) 
+} catch (err) {
+  console.error(err);
+  res.status(500).json({ message: "Unexpected server error" });
+}
+};
+
+// update function
+async function updateUser(req, res) {
+  try {
+    const updates = req.body
+    const updateUser = await User.findByIdAndUpdate(req.params.id, updates, {new: true})
+    if (!updateUser) {
+      return res.status(404).json({ message: "User not found" });
+  }
+  res.status(200).json({message: "User updated", user: updateUser}) 
+} catch (err) {
+  console.error(err);
+  res.status(500).json({ message: "Unexpected server error" });
+}
+};
+
+// findBy function
+async function findUser(req, res) {
+  try {
+    const userID = req.params.id
+    const foundUser = await User.findById(userID)
+    if (!foundUser) {
+      return res.status(404).json({message: "User not found"})
+    }
+    res.status(200).json({message: "User found", user: foundUser})
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Unexpected server error" });
+  }
 }
 
 const UsersController = {
-  create: create,
+  createUser: createUser,
+  deleteUser: deleteUser,
+  updateUser: updateUser,
+  findUser: findUser,
 };
 
 module.exports = UsersController;
