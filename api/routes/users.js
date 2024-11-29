30dede10e32bb01813451dfd7359933ce98fd4ae
Doug Fairfield <fairfielddouglas@gmail.com>
2024-11-29 17:02:39 +0000

    user token validation so far

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const tokenChecker = require("../middleware/tokenChecker");
 
 const UsersController = require("../controllers/users");
 
 const router = express.Router();
 
 router.post("/", UsersController.createUser);
-router.delete("/:id", UsersController.deleteUser);
-router.put("/:id", UsersController.updateUser);
+router.delete("/:id", tokenChecker, UsersController.deleteUser);
+router.put("/:id", tokenChecker, UsersController.updateUser);
 router.get("/:id", UsersController.findUser);
 
 module.exports = router;
