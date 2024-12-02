ff42c382aeec8b3d925f945b2f1de5ae75809b62
Khalid Ham <Khalidoham@gmail.com>
2024-12-02 11:35:29 +0000

    Services for alliances completed

--- frontend/src/services/alliances.js
+++ frontend/src/services/alliances.js
@@ -1,6 +1,6 @@
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-export const viewReceivedRequests = async (token, userId) => {
+export const viewReceivedRequests = async (token) => {
     const requestOptions = {
         method: "GET",
         headers: {
@@ -8,10 +8,67 @@ export const viewReceivedRequests = async (token, userId) => {
         },
     }
 
-    const response = await fetch(`${BACKEND_URL}/alliances/${userId}/viewReceivedRequests`, requestOptions);
+    const response = await fetch(`${BACKEND_URL}/alliances/viewReceivedRequests`, requestOptions);
 
     if (response.status != 200) {
         throw new Error("Unable to get alliances")
+    }
+
+    const data = await response.json();
+    return data
+}
+
+export const requestAlliance = async (token, receiverId) => {
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${token}`,
+            "content-type": "application/json"
+        },
+    }
+
+    const response = await fetch(`${BACKEND_URL}/alliances/${receiverId}`, requestOptions)
+    
+    if (response.status != 201) {
+        throw new Error("Unable to request alliance")
+    }
+
+    const data = await response.json();
+    return data
+}
+
+export const withdrawAllianceRequest = async (token, receiverId) => {
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${token}`,
+            "content-type": "application/json"
+        },
+    }
+
+    const response = await fetch(`${BACKEND_URL}/alliances/${receiverId}/cancel`, requestOptions)
+    
+    if (response.status != 200) {
+        throw new Error("Unable to cancel alliance request")
+    }
+
+    const data = await response.json();
+    return data
+}
+
+export const forgeAlliance = async (token, senderId) => {
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${token}`,
+            "content-type": "application/json"
+        },
+    }
+
+    const response = await fetch(`${BACKEND_URL}/alliances/${senderId}/forge`, requestOptions)
+    
+    if (response.status != 200) {
+        throw new Error("Unable to accept alliance request")
     }
 
     const data = await response.json();
