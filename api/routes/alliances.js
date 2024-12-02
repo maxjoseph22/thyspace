d6c324b5e96033de3dd44c827526aca40366f069
Khalid Ham <Khalidoham@gmail.com>
2024-12-02 10:47:29 +0000

    Controller now utilises tokens and route params to forge alliances

--- api/routes/alliances.js
+++ api/routes/alliances.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const { AllianceController } = require("../controllers/alliances");
 
-router.post("/", AllianceController.requestAlliance)
+router.post("/:id", AllianceController.requestAlliance)
 router.post("/:id/cancel", AllianceController.withdrawAllianceRequest)
 router.post("/:id/accept", AllianceController.acceptAlliance)
 router.get("/:id/receivedRequestsAdmin", AllianceController.viewReceivedRequestsAdmin)
-router.get("/:id/viewReceivedRequests", AllianceController.viewReceivedRequests)
+router.get("/viewReceivedRequests", AllianceController.viewReceivedRequests)
 
 module.exports = router;
