2bc491749981716a1a2325e9a9d5c288929d1836
Shola Forbes <shola.forbes1@gmail.com>
2024-12-03 16:02:54 +0000

    adding editing functions to logged in user profile page

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -56,7 +56,7 @@ async function getUserPosts(req, res){
   const {id} = req.params
   console.log("user id:", id)
   try{
-    const posts = await Post.find({ user_id: id}).sort({createdAt: -1});
+    const posts = await Post.find({ user_id: id}).sort({createdAt: -1}).populate('user_id', 'profilePicture username')
     const newToken = generateToken(req.user);
     res.status(200).json({posts: posts, token: newToken, });
   } catch (error) {
